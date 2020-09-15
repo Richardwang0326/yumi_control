746482f1433ffe5cb9339b1e2ae3a666ca035bdf
richard <kuolunwang@gmail.com>
2020-09-15 10:53:58 +0800

    bomb

--- yumi_arm_control.py
+++ yumi_arm_control.py
@@ -6,6 +6,8 @@ import cv2  # OpenCV module
 import time
 import message_filters
 import math
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 from sensor_msgs.msg import Image, CameraInfo
 import tf
 import tf.transformations as tfm
@@ -84,12 +86,39 @@ def Imageprocess(msg):
         cv2.CHAIN_APPROX_SIMPLE)
     
     cv2.drawContours(cv_image,contours,-1,(0,0,255),3)
+    
+    cv2.imshow("test", cv_image)
+    
     output_result.publish(cv_bridge.cv2_to_imgmsg(cv_image, encoding="passthrough"))
     # ====================================================================================================================================
     
     # ============================================
     # find point and quaternion
+    # =======Alex
+    total_x = 0
+    total_y = 0
+    count = 0
 
+    points = [[]]
+    for c in contours:
+        for c1 in c:
+            #print(c1)
+            for c2 in c1:
+                count += 1
+                total_x += c2[0]
+                total_y += c2[1]
+                
+            try:
+                points = np.insert(points, [len(points) - 1], c1,axis=0)
+            except:
+                points = np.insert(points, [0], c1,axis=1)
+            
+
+    center_x = total_x/count
+    center_y = total_y/count
+
+    center = center_x , center_y
+    # =======Alex
     for c in contours:
 	    rect = cv2.minAreaRect(c)
 
@@ -99,13 +128,18 @@ def Imageprocess(msg):
         angle=-rect[2]
 
     q = tf.transformations.quaternion_from_euler(0, 3.1415, 1.57-angle*3.1415/180, axes="rxyz")
+    center = [center_x, center_y]
 
-    return rect[0], q
+
+
+    return center, q
     # ============================================
+
+right_hand = [0.42,-0.906,-0.032,0.045]
 
 def transform_pose_to_base_link(t,q):
 
-    euler_pose = tfm.euler_from_quaternion(q)
+    euler_pose = tfm.euler_from_quaternion(right_hand)
     tf_cam_col_opt_fram = tfm.compose_matrix(translate=t, angles=euler_pose)
     
     trans, quat = listener.lookupTransform('base_link', 'camera_color_optical_frame', rospy.Time(0))
@@ -127,39 +161,105 @@ def summer_arm_control_cb(req):
     depth_msg = rospy.wait_for_message("/camera/aligned_depth_to_color/image_raw", Image)
     real_world_point = Finddepth(depth_msg, point)
 
+
     #quaternion = [0.004, 0.003, -0.923, -0.386]  # w x y z (please fix your quaternion)
-    position = transform_pose_to_base_link(real_world_point, q)
+    position = transform_pose_to_base_link(real_world_point, right_hand)
+
+    print(position[0])
+    print(position[1])
+    print(position[2])
 
     meter_to_mm = 1000
     req = GotoPoseRequest()
     req.wait_for_res = True
 
-    if position[1] >= 0 :
-        req.arm = 'left'
-    else:
-        req.arm = 'right'
 
-    req.quat = [q[3],q[0],q[1],q[2]]
 
-    req.position = [position[0]*meter_to_mm, position[1]*meter_to_mm, position[2]*meter_to_mm+50]
-    go_pose_plan_srv(req)
-    rospy.sleep(1)
-
-    move_gripper_srv(MoveGripperRequest(arm=req.arm, width=0.025))
-    rospy.sleep(1)
-
-    req.position = [position[0]*meter_to_mm, position[1]*meter_to_mm, position[2]*meter_to_mm+20]
-    go_pose_plan_srv(req)
-    rospy.sleep(1)
+    # left_hand move line
+    # ========================================================================================================
+    req.arm = 'left'
+    req.quat = [0.023,0.438,-0.899,-0.016]
 
     move_gripper_srv(MoveGripperRequest(arm=req.arm, width=0))
-    rospy.sleep(1)
-
-    req.position = [position[0]*meter_to_mm, position[1]*meter_to_mm, position[2]*meter_to_mm+50]
-    go_pose_plan_srv(req)
-    rospy.sleep(1)
+    rospy.sleep(0.01)
 
     goto_wait_joint_srv(req.arm)
+    
+    req.position = [0.38380000000000003*meter_to_mm, 0.047700000000000006*meter_to_mm, 0.13*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    req.position = [0.38380000000000003*meter_to_mm, 0.047700000000000006*meter_to_mm, 0.011*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    req.position = [0.3364*meter_to_mm, -0.0101*meter_to_mm, 0.011*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    req.position = [0.3005*meter_to_mm, 0.0*meter_to_mm, 0.011*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    req.position = [0.2642*meter_to_mm, 0.0341*meter_to_mm, 0.011*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+    # ======================================================================================================================
+
+    # right_hand move line
+    # ========================================================================================================
+    req.arm = 'right'
+    req.quat = [0.027,0.922,0.387,0.021]
+
+    goto_wait_joint_srv(req.arm)
+    move_gripper_srv(MoveGripperRequest(arm=req.arm, width=0))
+    rospy.sleep(0.01)
+
+    req.position = [0.4579*meter_to_mm, -0.09359999999999999*meter_to_mm, 0.15*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    req.position = [0.4579*meter_to_mm, -0.09359999999999999*meter_to_mm, 0.1*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+    req.position = [0.4579*meter_to_mm, -0.09359999999999999*meter_to_mm, 0.07*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    req.position = [0.4579*meter_to_mm, -0.09359999999999999*meter_to_mm, 0.038*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    move_gripper_srv(MoveGripperRequest(arm=req.arm, width=0.009))
+    rospy.sleep(0.01)
+
+    req.position = [0.4579*meter_to_mm, -0.09359999999999999*meter_to_mm, 0.011*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.01)
+
+    move_gripper_srv(MoveGripperRequest(arm=req.arm, width=0.0))
+    rospy.sleep(5.0)
+    # ===================================================================================================
+    # return to wait pose
+    # ===================================================================================================
+    move_gripper_srv(MoveGripperRequest(arm=req.arm, width=0.009))
+    rospy.sleep(2.0)
+    req.position = [0.4579*meter_to_mm, -0.09359999999999999*meter_to_mm, 0.15*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.2)
+
+    goto_wait_joint_srv(req.arm)
+
+    req.arm = 'left'
+    req.quat = [0.023,0.438,-0.899,-0.016]
+
+    req.position = [0.2642*meter_to_mm, 0.0341*meter_to_mm, 0.1*meter_to_mm]
+    go_pose_plan_srv(req)
+    rospy.sleep(0.5)
+
+    goto_wait_joint_srv(req.arm)
+
+    # ===================================================================================================
 
     return TriggerResponse()
 
@@ -176,5 +276,5 @@ if __name__=="__main__":
     goto_wait_joint_srv = rospy.ServiceProxy("/goto_wait_joint", YumipyTrigger)
     rospy.Service("~/summer_arm_control", Trigger, summer_arm_control_cb)
     output_result = rospy.Publisher('/camera/color/image_result', Image, queue_size=10)
-
+    
     rospy.spin()
